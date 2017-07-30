4c3172ad62ffcf87e02ac1bb2743e31f4fffcdc1
100297 <league@WTS7.attlocal.net>
2017-07-30 15:29:13 -0700

    I programmed a virtual robot to do many types of functions with java and processing.

--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,13 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+Robot Chumister= new Robot();
+Chumister.penDown();
+for(int i=0; i<4; i=i+1){
+Chumister.move(100);
+Chumister.turn(90);
 	
-		
+}
 		
 	}
 }
